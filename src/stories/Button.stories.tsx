6adf284099ca8c2d7e7d8a7f798d75962f0d26fb
codingwithmizan <mizan.samb@gmail.com>
2021-07-06 10:49:19 +0600

    change btn namr

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -13,8 +13,8 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const PrimaryBtn = Template.bind({});
-PrimaryBtn.args = {
+export const Primary = Template.bind({});
+Primary.args = {
   primary: true,
   label: 'Button',
 };
