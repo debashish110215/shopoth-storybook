015a4429da25fea7a319b5b2df57e62e2f49e3dc
debashish110215 <debashish110215@gmail.com>
2021-07-09 20:35:53 +0600

    change folder structure

---
+++ src/stories/components/CartButton.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import '../styles/cartButton.scss'
+import ClipLoader from "react-spinners/ClipLoader";
+
+export enum CartButtonState {
+    NORMAL,
+    LOADING,
+    DISABLED,
+    HIDDEN
+  }
+
+interface ButtonProps{
+    color?:'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger'| 'cart';
+    backgroundColor?:string;
+    outline?:boolean;
+    size?:'small' | 'medium' | 'large';
+    block?:string;
+    label:string;
+    onClick?:() => void;
+    state?:CartButtonState;
+    loaderColor?: 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger'| 'cart'|'light'|'dark';
+   
+}
+
+export const CartButton = ({
+    color = 'secondary',
+    outline = false,
+    size = 'medium',
+    backgroundColor,
+    loaderColor='success',
+    block ='block',
+    label, 
+    state = CartButtonState.NORMAL,
+    ...props
+}:ButtonProps) => {
+    const btnStyle = outline?`btn-outline-${color}`:`btn-${color}`
+    const loaderstyle = `text-${loaderColor}`
+    const isLoading = state === CartButtonState.LOADING
+    return (
+        <React.Fragment> 
+            {
+                state !== CartButtonState.HIDDEN &&
+                <button
+                    type='button'
+                    className={['btn', `btn-${size}`, `${block?'btn-block':''}`, btnStyle].join(' ')}
+                    style={{backgroundColor}}
+                    disabled={isLoading}
+                    {...props} > 
+                    {isLoading?  <ClipLoader color={loaderstyle} loading={isLoading}  size={10}/> : `${label}`}
+                </button>
+            }
+        </React.Fragment>
+    )
+}
---
+++ src/stories/components/Price.tsx
@@ -0,0 +1,36 @@
+import NumberFormat from 'react-number-format';
+import '../styles/price.scss'
+
+interface PriceProps{
+    currentPrice:number;
+    regularPrice?:number;
+    currentPriceSize?:number;
+    regularPriceSize?:number;
+    currency?:string
+
+}
+
+export const Price = ({regularPrice, currentPrice, currentPriceSize=1.2, regularPriceSize=0.9, currency='Tk'}:PriceProps) => {
+    return (
+        <div 
+        className={['price-container'].join(' ')}
+       
+        >
+            <div className='currentPrice'  style ={{fontSize:`${currentPriceSize}rem`}}>
+                <p>
+                {currency} <NumberFormat value={currentPrice} displayType={'text'} thousandSeparator={true} />
+                </p>
+            </div>
+            { regularPrice &&  
+            <div className='regularPrice'  style ={{fontSize:`${regularPriceSize}rem`}}>
+                <p>
+                {currency} <NumberFormat value={regularPrice} displayType={'text'} thousandSeparator={true} />
+                </p>
+            </div>
+            }
+          
+        </div>
+    )
+}
+
+
