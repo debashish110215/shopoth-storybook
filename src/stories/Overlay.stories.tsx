8c76fc7990c349c291096ae109bbb51bfcd59025
debashish110215 <debashish110215@gmail.com>
2021-07-10 15:11:58 +0600

    overlay fix

--- src/stories/Overlay.stories.tsx
+++ src/stories/Overlay.stories.tsx
@@ -1,20 +1,134 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { OverLay } from './components/Overlay'
+import { Overlay } from '../stories/components/Overlay'
 
 export default {
     title:'shop/Overlay',
-    component: OverLay,
+    component: Overlay,
     argTypes: {
         backgroundColor:{control:'color'}
     },
-} as ComponentMeta<typeof OverLay>
+} as ComponentMeta<typeof Overlay>
 
-const Template:ComponentStory<typeof OverLay> = (args) =><OverLay {...args}/>
+const Template:ComponentStory<typeof Overlay> = (args) =><Overlay {...args}/>
 
 export const BlankOverlay = Template.bind({});
 BlankOverlay.args = {
     onClose: () => {},
     children: <></>,
-    show: true
-  
+    show: true,
+    allowScroll: true
+};
+export const OverlayWithElement = Template.bind({});
+const abc =
+    <>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+        <br/>
+        <h1>Hello</h1>
+    </>
+
+OverlayWithElement.args = {
+    onClose: () => {},
+    children: abc,
+    show: true,
+    allowScroll: false
 };
--- src/stories/components/Overlay.tsx
+++ src/stories/components/Overlay.tsx
@@ -1,18 +1,33 @@
 import React, { useRef, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import '../styles/overlay.scss'
 
 interface OverLayProps {
     onClose: () => void;
-    children: React.ReactNode;
+    children: ReactNode;
     show: boolean;
+    allowScroll? : boolean;
 }
 
-export const OverLay = ({show = true, children, onClose}:OverLayProps) => { 
+export const Overlay = ({show = true, children, onClose, allowScroll = false}:OverLayProps) => { 
+    useEffect(()=> {
+        if (!allowScroll) {
+            let classes = document.querySelector("body").classList
+            classes.add("no-scroll")
+        }
+        return () => {
+            let classes = document.querySelector("body").classList
+            classes.remove("no-scroll")
+        };
+    })
     return (
         <React.Fragment >
             {
                 show &&
-                <div className="overlay" >
+                <div 
+                    className="overlay" 
+                    onClick={ (e) => { if (e.target === e.currentTarget) onClose() } }
+                    >
                     {children}
                 </div>
             }
