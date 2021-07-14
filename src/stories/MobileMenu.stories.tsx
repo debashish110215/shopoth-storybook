2ae1708f9fcbe3b6ea369ab676d0c72cbed2fd89
debashish110215 <debashish110215@gmail.com>
2021-07-14 18:05:35 +0600

    ready menu structure

---
+++ src/stories/MobileMenu.stories.tsx
@@ -0,0 +1,156 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { FaHireAHelper } from 'react-icons/fa';
+import { WrapperMegaMenu } from '../stories/components/MobileMenu'
+
+export default {
+    title:'shop/Mobile Mega Menu',
+    component: WrapperMegaMenu,
+    argTypes: {
+        backgroundColor:{control:'color'}
+    },
+} as ComponentMeta<typeof WrapperMegaMenu>
+
+const Template:ComponentStory<typeof WrapperMegaMenu> = (args) =><WrapperMegaMenu {...args}/>
+
+export const MobileMegaMenu = Template.bind({});
+MobileMegaMenu.args = {
+    customerServices: {
+        title : "Categories",
+        categories : [
+            {
+                icon : <FaHireAHelper/>,
+                title : "Phone ",
+                bnTitle: "Phone",
+                id : "1",
+                subCategory:[
+                    {
+                        title : "Phone 33",
+                        bnTitle: "Phone",
+                        id : "11",
+                        subCategory: []
+                    }
+                ]
+            }
+        ]
+    },
+    secondaryMenu: { 
+        title : "Shopoth.com",
+        categories : [
+            {
+                icon : <FaHireAHelper/>,
+                title : "Phone ",
+                bnTitle: "Phone",
+                id : "2",
+                subCategory:[
+                    {
+                        title : "Phone 44",
+                        bnTitle: "Phone",
+                        id : "21",
+                        subCategory: []
+                    }
+                ]
+            },
+            {
+                icon : <FaHireAHelper/>,
+                title : "Phone ",
+                bnTitle: "Phone",
+                id : "3",
+                subCategory:[
+                    {
+                        title : "Phone 44",
+                        bnTitle: "Phone",
+                        id : "31",
+                        subCategory: []
+                    }
+                ]
+            },
+            {
+                icon : <FaHireAHelper/>,
+                title : "Phone ",
+                bnTitle: "Phone",
+                id : "4",
+                subCategory:[
+                    {
+                        title : "Phone 44",
+                        bnTitle: "Phone",
+                        id : "41",
+                        subCategory: []
+                    }
+                ]
+            }
+        ]
+    },
+    primaryMenu : {
+        title : "Categories",
+        categories : [
+            {
+                title : "Phone ",
+                bnTitle: "Phone",
+                id : "5",
+                subCategory: [
+                    {
+                        title : "Phone 1",
+                        bnTitle: "Phone",
+                        id : "51",
+                        subCategory: []
+                    },
+                    {
+                        title : "Phone 2",
+                        bnTitle: "Phone",
+                        id : "52",
+                        subCategory: []
+                    },
+                    {
+                        title : "Phone 3",
+                        bnTitle: "Phone",
+                        id : "53",
+                        subCategory: []
+                    },
+                    {
+                        title : "Phone 4",
+                        bnTitle: "Phone",
+                        id : "54",
+                        subCategory: []
+                    }
+                ]
+            },
+            {
+                title : "Phone 2",
+                bnTitle: "Phone",
+                id : "6",
+                subCategory: [
+                    {
+                        title : "Phone 21",
+                        bnTitle: "Phone",
+                        id : "61",
+                        subCategory: []
+                    },
+                    {
+                        title : "Phone 22",
+                        bnTitle: "Phone",
+                        id : "62",
+                        subCategory: []
+                    },
+                    {
+                        title : "Phone 23",
+                        bnTitle: "Phone",
+                        id : "63",
+                        subCategory: []
+                    },
+                    {
+                        title : "Phone 24",
+                        bnTitle: "Phone",
+                        id : "64",
+                        subCategory: []
+                    }
+                ]
+            },
+            {
+                title : "Phone 3",
+                bnTitle: "Phone",
+                id : "7",
+                subCategory: [ ]
+            }
+        ]
+    }
+};
--- src/stories/components/MobileMenu.tsx
+++ src/stories/components/MobileMenu.tsx
@@ -1,36 +1,21 @@
 import { FC, useEffect, useState } from "react";
-import '../styles/megaMenu.scss'
+import '../styles/mobileMegaMenu.scss'
 import { Overlay } from "./Overlay";
-import {FiChevronRight} from 'react-icons/fi'
-import { IconType } from "react-icons";
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
+import { MegaMenu, MenuProps, MenuState } from "./Menu";
+import { useRef } from "react";
 
-export enum MenuState {
-    OPEN,
-    CLOSED
+export enum Direction {
+    FORWARD,
+    BACK
 }
 
-export interface Category {
-    title : string
-    id : string
-    bnTitle : string
-    subCategory : Category []
-    icon?: IconType
-}
-
-export interface MegaMenu {
-    title: string
-    categories : Category []
-}
-
-export interface MenuProps {
-    primaryMenu : MegaMenu
-    secondaryMenu : MegaMenu
-    customerServices : MegaMenu
-    menuState : MenuState
+export enum Level {
+    LEVEL1,
+    LEVEL2
 }
 
 export interface MenuLevel1Props {
-    menuState: MenuState
     menu : MegaMenu
     selectedMenuId : string
     onMenuSelect : (id: string) => void
@@ -38,25 +23,34 @@ export interface MenuLevel1Props {
 
 export interface MenuLevel2Props {
     menu : MegaMenu
-    menuState: MenuState
     selectedMenuId : string
+    onBackClick : (id: any) => void
 }
 
-export const MenuLevel1:FC<MenuLevel1Props>  = ({menu, selectedMenuId, onMenuSelect, menuState = MenuState.OPEN}: MenuLevel1Props) => {
+export const MenuLevel1:FC<MenuLevel1Props>  = ({menu, selectedMenuId, onMenuSelect}: MenuLevel1Props) => {
     console.log("selectedMenuId"+selectedMenuId)
     return (
-        <div className={['menu-level level1', `${menuState === MenuState.OPEN ? 'd-block': 'd-none'}`].join(' ')}>
+        <div className={['menu-level level1'].join(' ')}>
             <h1 className="menu-title">{menu.title}</h1>
              <ul className="menu-ul">
                 {
                     menu.categories.map(item=>{
                         return (
-                            <li key={item.title} className={['menu-li',selectedMenuId === item.id ? 'active' : ''].join(' ')} onMouseOver={()=>onMenuSelect(item.id)}>
-                                <p>{item.icon} {item.title}</p>
-                                {
-                                    item.subCategory.length>0&&<span><FiChevronRight/></span>
-                                }
-                            </li>
+                            <>
+                            {
+                                item.subCategory.length>0 ?
+                                <li key={item.title} className={['menu-li',selectedMenuId === item.id ? 'active' : ''].join(' ')} onClick={()=>onMenuSelect(item.id)}>
+                                    <p>{item.icon} {item.title}</p>
+                                    {
+                                        item.subCategory.length>0&&<span><FiChevronRight/></span>
+                                    }
+                                </li>
+                                :
+                                <li key={item.title} className={['menu-li',selectedMenuId === item.id ? 'active' : ''].join(' ')}>
+                                    <p>{item.icon} {item.title}</p>
+                                </li>
+                            }
+                            </>
                         )
                     })
                 }
@@ -65,12 +59,13 @@ export const MenuLevel1:FC<MenuLevel1Props>  = ({menu, selectedMenuId, onMenuSel
     )
 }
 
-export const MenuLevel2:FC<MenuLevel2Props>  = ({menu, menuState = MenuState.OPEN, selectedMenuId}: MenuLevel2Props) => {
+export const MenuLevel2:FC<MenuLevel2Props>  = ({menu, selectedMenuId, onBackClick}: MenuLevel2Props) => {
     let subItem = menu.categories.find(i => i.id === selectedMenuId)
     return (
         <>
             { subItem && subItem.subCategory.length > 0 && 
-                <div className={['menu-level level2', `${selectedMenuId && menuState === MenuState.OPEN ? 'd-block': 'd-none'}`].join(' ')}>
+                <div className={['menu-level level2'].join(' ')}>
+                    <p className={"back-to-main"} onClick={(e)=>{onBackClick(subItem?.id)}}><FiChevronLeft/> Back to main</p>
                     <h1 className="menu-title"><a className="menu-category" href={subItem.id}>{subItem.title}</a></h1>
                     <ul className="menu-ul">
                         {
@@ -83,31 +78,53 @@ export const MenuLevel2:FC<MenuLevel2Props>  = ({menu, menuState = MenuState.OPE
                             })
                         }
                     </ul>
-                    {/* <div><h1>Banner</h1></div> */}
                 </div>
             }
         </>
     )
 }
 
-export const MegaMenuDesktop:FC<MenuProps> = ({primaryMenu, secondaryMenu, customerServices, menuState}:MenuProps) => {
+export const MegaMenuMobile:FC<MenuProps> = ({primaryMenu, secondaryMenu, customerServices, menuState}:MenuProps) => {
     const [selectedId, setSelectedId] = useState("")
+    const [class1, setClass1] = useState('center')
+    const [class2, setClass2] = useState('right')
+
+    let menuLevel1Ref = useRef<HTMLDivElement>(null)
+    let menuLevel2Ref = useRef<HTMLDivElement>(null)
+
+    const scrollTop = () => {
+        if(menuLevel1Ref.current) menuLevel1Ref.current.scrollTop = 0
+        if(menuLevel2Ref.current) menuLevel2Ref.current.scrollTop = 0
+    }
+
+    const onBackClick = (id: string) => {
+        scrollTop()
+        scrollTop()
+        setSelectedId(id)
+        setClass1('center')
+        setClass2('right')
+    }
 
     let menuSelect = (id: string) => {
+        scrollTop()
         setSelectedId(id)
+        setClass1('left')
+        setClass2('center')
     }
 
     useEffect(()=>{
         setSelectedId("")
+        setClass1('center')
+        setClass2('right')
     }, [menuState])
 
     return (
-        <div className="mega-menu">
+        // if one ref here back to that postion
+        <div className="mega-mobile-menu"> 
             {
                 menuState === MenuState.OPEN &&
-                <div className="mega-menu-level1">
-                    <MenuLevel1 
-                        menuState = {menuState}
+                <div ref = {menuLevel1Ref} className={['mega-menu-level1',class1].join(' ')}>
+                    <MenuLevel1
                         menu = { 
                             {
                                 categories:  secondaryMenu.categories,
@@ -117,8 +134,7 @@ export const MegaMenuDesktop:FC<MenuProps> = ({primaryMenu, secondaryMenu, custo
                         selectedMenuId = {selectedId}
                         onMenuSelect = {menuSelect}
                     />
-                    <MenuLevel1 
-                        menuState = {menuState}
+                    <MenuLevel1
                         menu = { 
                             {
                                 categories:  primaryMenu.categories,
@@ -128,8 +144,7 @@ export const MegaMenuDesktop:FC<MenuProps> = ({primaryMenu, secondaryMenu, custo
                         selectedMenuId = {selectedId}
                         onMenuSelect = {menuSelect}
                     />
-                    <MenuLevel1 
-                        menuState = {menuState}
+                    <MenuLevel1
                         menu = { 
                             {
                                 categories:  customerServices.categories,
@@ -143,10 +158,10 @@ export const MegaMenuDesktop:FC<MenuProps> = ({primaryMenu, secondaryMenu, custo
             }
             
             {
-                menuState === MenuState.OPEN && selectedId &&
-                <div className="mega-menu-level2">
-                    <MenuLevel2 
-                        menuState = {menuState}
+                menuState === MenuState.OPEN && 
+                <div ref = {menuLevel2Ref} className={['mega-menu-level2',class2].join(' ')}>
+                    <MenuLevel2
+                        onBackClick = {onBackClick}
                         menu = { 
                             {
                                 categories:  secondaryMenu.categories,
@@ -156,8 +171,8 @@ export const MegaMenuDesktop:FC<MenuProps> = ({primaryMenu, secondaryMenu, custo
                         selectedMenuId = {selectedId}
                     />
                     
-                    <MenuLevel2 
-                        menuState = {menuState}
+                    <MenuLevel2
+                        onBackClick = {onBackClick}
                         menu = { 
                             {
                                 categories:  primaryMenu.categories,
@@ -167,8 +182,8 @@ export const MegaMenuDesktop:FC<MenuProps> = ({primaryMenu, secondaryMenu, custo
                         selectedMenuId = {selectedId}
                     />
                     
-                    <MenuLevel2 
-                        menuState = {menuState}
+                    <MenuLevel2
+                        onBackClick = {onBackClick}
                         menu = { 
                             {
                                 categories:  customerServices.categories,
@@ -203,7 +218,7 @@ export const WrapperMegaMenu = ({primaryMenu, secondaryMenu, customerServices}:a
             children={
                 <div>
                 <a onClick={onClickClose}>{menuStates===MenuState.CLOSED?"open":"close"}</a>
-                <MegaMenuDesktop  primaryMenu = {primaryMenu} secondaryMenu={secondaryMenu} customerServices={customerServices} menuState={menuStates} />
+                <MegaMenuMobile  primaryMenu = {primaryMenu} secondaryMenu={secondaryMenu} customerServices={customerServices} menuState={menuStates} />
             </div>
             }
             show={true}
